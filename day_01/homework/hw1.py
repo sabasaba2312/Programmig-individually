841cf1ab5754402af9f5234dae4eb2ebb5bfb177
sabasaba2312 <sabapro66@gmail.com>
2026-04-05 16:50:14 +0400

    second commit

---
+++ day_01/homework/hw1.py
@@ -0,0 +1,4 @@
+#2)შექმენი ცვლადი my_text და მიაწერე მას ტექსტი "Hello, World!". დაბეჭდე ცვლადის ტიპი.
+my_text = "Hello, World!"
+
+print(type(my_text))
---
+++ day_01/homework/hw3.py
@@ -0,0 +1,6 @@
+#4)შექმენი ორი ცვლადი first_name და last_name და მიაწერე მათ შესაბამისი სახელები. დაბეჭდე ორივე ერთად.
+
+first_name = "saba"
+last_name = "tavartkiladze"
+
+print(first_name + " " + last_name)
