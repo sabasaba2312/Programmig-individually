c0963823c99b985dae06cb543fae0967c1c0c533
sabasaba2312 <sabapro66@gmail.com>
2026-04-11 15:30:33 +0400

    second commit

---
+++ day_02/homework/hw.py
@@ -0,0 +1,9 @@
+# მომხმარებელს შემოატანინეთ 2 რიცხვი, ხოლო შემდეგ გაკეთეთ ამ რიცხვებზე ყველა მათემატიკური ოპერაცია ცალცალკე.
+
+num1 = int(input("enter your number: "))
+num2 = int(input("enter your number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 // num2)
---
+++ day_02/homework/hw2.py
@@ -0,0 +1,6 @@
+# მომხმარებელს შემოატანინეთ თავისი სახელი და გავარი, შემდეგ კი ტერმინალში გამოიტანეთ მომხმარებლის შემოტანილი მნიშვნელობები
+
+user_name = input("enter your name: ").strip().capitalize()
+user_surname = input("enter your surname: ").strip().capitalize()
+
+print(user_name,user_surname)
